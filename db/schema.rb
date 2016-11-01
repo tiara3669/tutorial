798b5159691e8e4b910347c35f148f75ebbc6075
Marcus Teo <tiara3669@yahoo.com>
2016-11-02 00:12:59 +0800

    First Trial

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101151833) do
+ActiveRecord::Schema.define(version: 20161101153837) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
